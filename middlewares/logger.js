0421189bc700d77951c537c6853610e9368490e7
Brittni Carter <bfcarter@fullsail.edu>
2017-01-08 20:21:51 -0600

    console.log

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
     var ip = req.ip;
     var realUrl = req.originalUrl;
 
-    console.log("ip: ", ip, " sent an request to url: ", realUrl);
+    console.log("ip: ", ip, " sent an request to url: ", realUrl); // print ip: sent an request to url 
 
     next();
 };
